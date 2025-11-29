2f614e10c0d1ec1ee97d1197235f8c4b08ada9f5
crosti12 <cjsuterland@gmail.com>
2025-11-29 05:13:28 -0400

    cors returned

--- index.js
+++ index.js
@@ -18,14 +18,18 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, false);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    optionsSuccessStatus: 200,
-    maxAge: 86400,
   })
 );
 
-app.options(/.*/, cors());
 app.use(express.json());
 app.use(rateLimit({ windowMs: 60 * 1000, max: 100 }));
 
